1b338eddb707d49cdcd69fc861a0f52404d6c8d5
Flavio Lopes <flaviotxa@gmail.com>
2021-07-15 16:48:38 +0100

    backend bootstrap

---
+++ src/database/index.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+
+class Database {
+  constructor() {
+    this.mongo();
+  }
+
+  mongo() {
+    this.mongoConnection = mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useFindAndModify: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log('Connected successfully to mongo server');
+  }
+}
+
+export default new Database();
